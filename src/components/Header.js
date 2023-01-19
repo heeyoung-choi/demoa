748cfb17a52a7b8a3f87e3a74096abb60647c2de
heeyoung-choi <oprocinante@gmail.com>
2023-01-19 23:11:08 +0700

    change class

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ export class Header extends Component
                 <div>
                     <img className="header-img" src={dangbee}></img>
                 </div>
-                <div><i class="header-heart fa-solid fa-heart-pulse"></i></div>
+                <div><i className="header-heart fa-solid fa-heart-pulse"></i></div>
                 <div>
                     <img className="header-img" src={nhu1}></img>
                 </div>
